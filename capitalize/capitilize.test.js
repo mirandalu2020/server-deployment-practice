620ffc4355b4d44bcfa8e688ec32baba14b40b82
mirandalu2020 <yuanling1007@hotmail.com>
2023-04-03 14:07:08 -0700

    demo files added

---
+++ capitalize/capitilize.test.js
@@ -0,0 +1,10 @@
+const capitalize = require('./capitilize.js');
+
+describe('As a User I want a way to capitalize my strings', () => {
+
+  // our unit test
+  test('Should capitalize a string', () => {
+    let output = capitalize('jacob');
+    expect(output).toEqual('JACOB');
+  });
+});
